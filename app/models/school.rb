df72e0bff8d617dc8fddc50945e866defb48e82f
Jeffery Gontarek <jeffery@jeffsmac.local>
2020-01-24 00:29:39 -0800

    stuck on invalid params tests

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,5 +1,5 @@
 class School < ApplicationRecord
-  has_many :student, dependent: :destroy
+  has_many :students, dependent: :destroy
 
   validates :capacity, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 52000 }
   validates :name, uniqueness: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,14 +3,14 @@ School.destroy_all
 100.times do |i|
   school = School.create!({
     name: "School #{i}",
-    address: '123 Street',
-    principal: 'Mr. #{i}',
+    address: "123 Street BLVD",
+    principal: "Mr. #{i}",
     capacity: i + 1 * 10,
     private_school: (rand(i) % 2 == 0)
   })
 
   i.times do |j|
-    school.student.create!({
+    school.students.create!({
       name: "Student #{i}#{j}",
       student_number: i + j * 23,
       gpa: i * j + rand
