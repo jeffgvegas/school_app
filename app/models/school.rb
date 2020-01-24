9092e62f6d2db21138021f8fb16536cbd52dff6a
Jeffery Gontarek <jeffery@jeffsmac.local>
2020-01-24 01:34:39 -0800

    VICTORY all specs PASS, also added rails_admin, was stuck on simple validation presence: true  lolgit status

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,6 +1,11 @@
 class School < ApplicationRecord
   has_many :students, dependent: :destroy
 
-  validates :capacity, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 52000 }
-  validates :name, uniqueness: true
+  validates :capacity, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 52000 }
+  validates :name, uniqueness: true, presence: true
+  validates :address, presence: true
+  validates :principal, presence: true
+  
+@school = School.new(name: name)
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: "schools#index"
   resources :schools 
   resources :students
